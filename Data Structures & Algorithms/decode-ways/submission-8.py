26d064a9bd0e4a3f5436a52f65fb0675d6894e95
alkazer26 <rhushilvasavada@icloud.com>
2026-08-13 11:21:10 -0500

    Add: decode-ways - submission-8

---
+++ Data Structures & Algorithms/decode-ways/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # don't count number of groups, count number of ways to group
+        memo = {}
+
+        def recurse(i):
+            if i in memo:
+                return memo[i]
+                
+            if i == len(s):
+                return 1
+
+            total = 0
+            if i <= len(s) - 1 and s[i] != "0":
+                total += recurse(i + 1)
+            if i + 1 < len(s) and 10 <= int(s[i] + s[i + 1]) <= 26:
+                total += recurse(i + 2)
+            
+            memo[i] = total
+            return memo[i]
+
+        return recurse(0)
