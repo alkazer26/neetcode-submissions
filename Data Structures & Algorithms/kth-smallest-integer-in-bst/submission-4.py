8121892aad39a8a33f372c7b33bbf18fb381a1d0
alkazer26 <rhushilvasavada@icloud.com>
2026-08-30 17:40:31 -0500

    Add: kth-smallest-integer-in-bst - submission-4

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-4.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # TODO: implement recursive DFS, each time we reach base case increment idx, check idx == k?
+        # implement BFS, somehow count the idx? store it in the bfs queue? idk.
+        idx = 0
+        kth = None
+        def recurse(node):
+            nonlocal idx
+            nonlocal k
+            nonlocal kth
+
+            if not node:
+                return 
+            
+            recurse(node.left)
+
+            idx += 1
+
+            if idx == k:
+                kth = node.val
+
+            recurse(node.right)
+        
+        recurse(root)
+
+        return kth
