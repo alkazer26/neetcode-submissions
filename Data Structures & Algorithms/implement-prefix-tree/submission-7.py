1be9c226694c774d262fb0a74d67b893fc735d22
alkazer26 <rhushilvasavada@icloud.com>
2026-08-31 17:30:36 -0500

    Add: implement-prefix-tree - submission-7

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-7.py
@@ -0,0 +1,65 @@
+class TrieNode:
+    def __init__(self, val="", isLast=False, nxt=None):
+        self.val = val 
+        if nxt is None:
+            self.nxt = {} # {"char" : "TrieNode"}
+        else:
+            self.nxt = nxt
+            
+        self.isLast = isLast
+
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def insert(self, word: str) -> None:
+        i = 0
+        cur = self.root
+
+        while i < len(word):
+            if word[i] in cur.nxt:
+                cur = cur.nxt[word[i]]
+            else:
+                last_flag = i == len(word) - 1
+                cur.nxt[word[i]] = TrieNode(word[i], last_flag)
+                cur = cur.nxt[word[i]]
+
+            i += 1
+        
+        cur.isLast = True
+        
+    def search(self, word: str) -> bool:
+        i = 0
+        cur = self.root
+
+        while i < len(word):
+            if word[i] not in cur.nxt:
+                return False
+            else:
+                cur = cur.nxt[word[i]]
+
+            i += 1
+        
+        return cur.isLast
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        i = 0
+        cur = self.root
+
+        while i < len(prefix):
+            if prefix[i] not in cur.nxt:
+                return False
+            else:
+                cur = cur.nxt[prefix[i]]
+
+            i += 1
+
+        return prefix[i - 1] == cur.val
+
+
+        
+        
