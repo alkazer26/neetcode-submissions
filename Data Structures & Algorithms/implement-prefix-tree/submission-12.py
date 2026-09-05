56992b3187a0172c06c3615042dba5078797ebbf
alkazer26 <rhushilvasavada@icloud.com>
2026-09-05 14:00:34 -0500

    Add: implement-prefix-tree - submission-12

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-12.py
@@ -0,0 +1,55 @@
+class TrieNode:
+    def __init__(self, isLast=False, nxt=None):
+        if nxt is None:
+            nxt = {}
+            
+        self.nxt = nxt  # {"char" : "TrieNode"}
+        self.isLast = isLast
+
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        i = 0
+        cur = self.root
+
+        while i < len(word):
+            if word[i] in cur.nxt:
+                cur = cur.nxt[word[i]]
+            else:
+                cur.nxt[word[i]] = TrieNode()
+                cur = cur.nxt[word[i]]
+
+            i += 1
+
+        cur.isLast = True
+
+    def search(self, word: str) -> bool:
+        i = 0
+        cur = self.root
+
+        while i < len(word):
+            if word[i] not in cur.nxt:
+                return False
+            else:
+                cur = cur.nxt[word[i]]
+
+            i += 1
+
+        return cur.isLast
+
+    def startsWith(self, prefix: str) -> bool:
+        i = 0
+        cur = self.root
+
+        while i < len(prefix):
+            if prefix[i] not in cur.nxt:
+                return False
+            else:
+                cur = cur.nxt[prefix[i]]
+
+            i += 1
+
+        return True
